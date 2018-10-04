f34bdbddfa38eb4c6239cb06b02abb0e59e8fc2c
uday97 <uday.agg97@gmail.com>
2018-10-04 14:09:59 +0530

    Real time stream now being stored

--- neural_style_transfer_video.py
+++ neural_style_transfer_video.py
@@ -37,55 +37,61 @@ net = cv2.dnn.readNetFromTorch(modelPath)
 # initialize the video stream, then allow the camera sensor to warm up
 print("[INFO] starting video stream...")
 vs = VideoStream(src=0).start()
+
 time.sleep(2.0)
+
+out = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc(*'MJPG'), 3.0, (640,480))
+
 print("[INFO] {}. {}".format(modelID + 1, modelPath))
 
 # loop over frames from the video file stream
 while True:
 	# grab the frame from the threaded video stream
-	frame = vs.read()
-
+    frame = vs.read()
+    out.write(frame)
+    
 	# resize the frame to have a width of 600 pixels (while
 	# maintaining the aspect ratio), and then grab the image
 	# dimensions
-	frame = imutils.resize(frame, width=600)
-	orig = frame.copy()
-	(h, w) = frame.shape[:2]
+    frame = imutils.resize(frame, width=600)
+    orig = frame.copy()
+    (h, w) = frame.shape[:2]
 
 	# construct a blob from the frame, set the input, and then perform a
 	# forward pass of the network
-	blob = cv2.dnn.blobFromImage(frame, 1.0, (w, h),
-		(103.939, 116.779, 123.680), swapRB=False, crop=False)
-	net.setInput(blob)
-	output = net.forward()
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (w, h),
+                                 (103.939, 116.779, 123.680), swapRB=False, crop=False)
+    net.setInput(blob)
+    output = net.forward()
 
 	# reshape the output tensor, add back in the mean subtraction, and
 	# then swap the channel ordering
-	output = output.reshape((3, output.shape[2], output.shape[3]))
-	output[0] += 103.939
-	output[1] += 116.779
-	output[2] += 123.680
-	output /= 255.0
-	output = output.transpose(1, 2, 0)
+    output = output.reshape((3, output.shape[2], output.shape[3]))
+    output[0] += 103.939
+    output[1] += 116.779
+    output[2] += 123.680
+    output /= 255.0
+    output = output.transpose(1, 2, 0)
 
 	# show the original frame along with the output neural style
 	# transfer
-	cv2.imshow("Input", frame)
-	cv2.imshow("Output", output)
-	key = cv2.waitKey(1) & 0xFF
+    cv2.imshow("Input", frame)
+    cv2.imshow("Output", output)
+    key = cv2.waitKey(1) & 0xFF
 
 	# if the `n` key is pressed (for "next"), load the next neural
 	# style transfer model
-	if key == ord("n"):
+    if key == ord("n"):
 		# grab the next nueral style transfer model model and load it
-		(modelID, modelPath) = next(modelIter)
-		print("[INFO] {}. {}".format(modelID + 1, modelPath))
-		net = cv2.dnn.readNetFromTorch(modelPath)
+        (modelID, modelPath) = next(modelIter)
+        print("[INFO] {}. {}".format(modelID + 1, modelPath))
+        net = cv2.dnn.readNetFromTorch(modelPath)
 
 	# otheriwse, if the `q` key was pressed, break from the loop
-	elif key == ord("q"):
-		break
+    elif key == ord("q"):
+        break
 
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
+out.release()
